55cae7a3ccb6caa9cdaf4f9b576df7d5f1992b6e
Pedro312 <pedro47.596@gmail.com>
2016-12-09 15:41:16 -0800

    Initial Upload
    12/9/16

---
+++ Lucky Sevens.py
@@ -0,0 +1,19 @@
+###
+#Lucky Sevens
+###
+
+# Start with $10, every game costs $1 to play
+# Roll two dice
+# If the result is seven, win $5 (+$4 overall after the $1 cost).
+# The game continously plays until you run out of money.
+# At the end, it tells you how many rounds were played
+
+import random
+import sys
+money = 10
+dice1 = random.randint (1, 6)
+dice2 = random.randint (1, 6)
+
+while money!=0
+if total ==7:
+    money + 4
